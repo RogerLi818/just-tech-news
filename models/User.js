f55bd3497172ed9c4e71e5036ab24ab1d546e0b5
RogerLi818 <jackliteng@yahoo.com>
2022-06-30 00:15:23 -0700

    add hash

--- models/User.js
+++ models/User.js
@@ -1,10 +1,16 @@
-const bcrypt = require('bcrypt');
 const {Model, DataTypes} = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 
 // create your user model
-class User extends Model{}
+class User extends Model{
+    //set up method to run on instance data (per user) to check password
+    checkPassword(loginPw){
+        //this represent user's properties, including password.
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 // define table columns and configuration
 User.init(
